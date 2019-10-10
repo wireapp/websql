31dd7d032084b0371492fc0279d118377a9dc851
Florian Keller <ffflorian@users.noreply.github.com>
2019-10-10 11:36:07 +0200

    chore: Format definition file (#24)

--- websql.d.ts
+++ websql.d.ts
@@ -1,26 +1,38 @@
-import { SQLite } from "./packages/worker/src/Helper";
-import {
-  ExecResultInterface,
-  ParamsInterface,
-  ConnectionOptions
-} from "./packages/worker/src/DatabaseInteface";
-import {
-  ResultGetType,
-  BindType
-} from "./packages/worker/src/StatementInterface";
-import { DatabaseWorkerOptions } from "./packages/wrapper/src/WorkerInterface";
+/*
+ * Wire
+ * Copyright (C) 2019 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+import {ConnectionOptions, ExecResultInterface, ParamsInterface} from './packages/worker/src/DatabaseInteface';
+import {SQLite} from './packages/worker/src/Helper';
+import {BindType, ResultGetType} from './packages/worker/src/StatementInterface';
+import {DatabaseWorkerOptions} from './packages/wrapper/src/WorkerInterface';
 
 // All public functions from Statement are promisified in the proxy
 // Statement cannot be instantiated from outside so we can remove the constructor here
 declare class Statement {
   bind(values: BindType): Promise<boolean>;
-  step(): Promise<boolean | void>;
-  get(params?: BindType): Promise<ResultGetType>;
-  getColumnNames(): Promise<string[]>;
-  getAsObject(): Promise<{ [key: string]: any }[]>;
-  run(values: BindType): Promise<void>;
-  reset(): Promise<boolean>;
   free(): Promise<boolean>;
+  get(params?: BindType): Promise<ResultGetType>;
+  getAsObject(): Promise<{[key: string]: any}[]>;
+  getColumnNames(): Promise<string[]>;
+  reset(): Promise<boolean>;
+  run(values: BindType): Promise<void>;
+  step(): Promise<boolean | void>;
 }
 
 // Because Database in Wrapper is a proxy to the Database class in Worker we have to
@@ -29,18 +41,14 @@ declare class Statement {
 export declare class Database {
   constructor(workerUrl: string, options?: DatabaseWorkerOptions);
   _getWorkerInstance(): Promise<Worker>;
-  mount(
-    options: ConnectionOptions,
-    identifier?: string,
-    nodeDatabaseDir?: string
-  ): Promise<void>;
   close(saveAfterClose?: boolean): Promise<void>;
-  saveChanges(): Promise<void>;
-  run(query: string, params?: ParamsInterface): Promise<void>;
   execute(query: string): Promise<ExecResultInterface[]>;
-  export(encoding?: "binary"): Promise<Uint8Array>;
-  export(encoding: "utf8"): Promise<string>;
-  wipe(identifier: string): Promise<void>;
+  export(encoding: 'utf8'): Promise<string>;
+  export(encoding?: 'binary'): Promise<Uint8Array>;
   getRowsModified(): Promise<number>;
+  mount(options: ConnectionOptions, identifier?: string, nodeDatabaseDir?: string): Promise<void>;
   prepare(query: string, params?: ParamsInterface): Promise<Statement>;
+  run(query: string, params?: ParamsInterface): Promise<void>;
+  saveChanges(): Promise<void>;
+  wipe(identifier: string): Promise<void>;
 }
